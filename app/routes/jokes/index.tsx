6c7041a0879f9c6d3fbf351ebcb1d464a3a1c9fe
Ogun Babacan <ogun.babacan@trendyol.com>
2022-01-15 02:29:20 +0300

    feat: getServerSideProps eq

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,11 +1,30 @@
-export default function JokesIndexRoute() {
+import type { LoaderFunction } from "remix";
+import { Link, useLoaderData } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async (): Promise<LoaderData> => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+
+  const data: LoaderData = { joke: randomJoke };
+  return data;
+};
+
+export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
-      <p>Here's a random joke:</p>
-      <p>
-        I was wondering why the frisbee was getting bigger,
-        then it hit me.
-      </p>
+      <p>Here's your hilarious joke:</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
 }
