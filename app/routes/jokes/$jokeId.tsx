ff630f26460510524755350df4a9ca9096e5df5c
Ogun Babacan <ogun.babacan@trendyol.com>
2022-01-21 23:33:50 +0300

    feat: optimistic UI

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -2,7 +2,8 @@ import type { Joke } from "@prisma/client";
 import { ActionFunction, Form, LoaderFunction, MetaFunction } from "remix";
 import { Link, useLoaderData, useCatch, redirect, useParams } from "remix";
 import { db } from "~/utils/db.server";
-import { requireUserId } from "~/utils/session.server";
+import { getUserId, requireUserId } from "~/utils/session.server";
+import { JokeDisplay } from "~/components/joke";
 
 export const meta: MetaFunction = ({
   data,
@@ -21,18 +22,22 @@ export const meta: MetaFunction = ({
   };
 };
 
-type LoaderData = { joke: Joke };
+type LoaderData = { joke: Joke; isOwner: boolean };
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const userId = await getUserId(request);
+
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
+
   if (!joke) {
     throw new Response("What a joke! Not found.", {
       status: 404,
     });
   }
-  const data: LoaderData = { joke };
+
+  const data: LoaderData = { joke, isOwner: userId === joke.jokesterId };
   return data;
 };
 
@@ -59,19 +64,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
 
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-      <Form method="post">
-        <input type="hidden" name="_method" value="delete" />
-        <button type="submit" className="button">
-          Delete
-        </button>
-      </Form>
-    </div>
-  );
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />;
 }
 
 export function CatchBoundary() {
